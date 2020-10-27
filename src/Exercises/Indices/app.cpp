4b0532c5baf6f4693c7f1dd8389061665d17c649
Kubpica <kubpicapf@gmail.com>
2020-10-27 02:25:48 +0100

    Bufor indeksów dodany i powtarzające się wierzchołki usunięto

--- src/Exercises/Indices/app.cpp
+++ src/Exercises/Indices/app.cpp
@@ -25,18 +25,16 @@ void SimpleShapeApplication::init() {
 
     std::vector<GLfloat> vertices = {
             // Dach - trójk¹t
-            -0.8f, 0.2f, 0.0f, 1.0f, 0.0f, 0.0f,
-            0.8f, 0.2f, 0.0f, 1.0f, 0.0f, 0.0f,
-            0.0f, 0.6f, 0.0f, 1.0f, 0.0f, 0.0f,
+            -0.8f, 0.2f, 0.0f, 1.0f, 0.0f, 0.0f, // 0
+            0.8f, 0.2f, 0.0f, 1.0f, 0.0f, 0.0f, // 1
+            0.0f, 0.6f, 0.0f, 1.0f, 0.0f, 0.0f, // 2
             // Blok - kwadrat z dwóch trójk¹tów
             // Lewy trójk¹t
-            -0.4f, -0.6f, 0.0f, 1.0f, 1.0f, 0.0f,
-            0.4f, -0.6f, 0.0f, 1.0f, 1.0f, 0.0f,
-            -0.4f, 0.2f, 0.0f, 1.0f, 1.0f, 0.0f,
+            -0.4f, -0.6f, 0.0f, 1.0f, 1.0f, 0.0f, // 3
+            0.4f, -0.6f, 0.0f, 1.0f, 1.0f, 0.0f, // 4
+            -0.4f, 0.2f, 0.0f, 1.0f, 1.0f, 0.0f, // 5
             // Prawy trójk¹t
-            -0.4f, 0.2f, 0.0f, 1.0f, 1.0f, 0.0f,
-            0.4f, -0.6f, 0.0f, 1.0f, 1.0f, 0.0f,
-            0.4f, 0.2f, 0.0f, 1.0f, 1.0f, 0.0f
+            0.4f, 0.2f, 0.0f, 1.0f, 1.0f, 0.0f // 6
     };
 
     GLuint v_buffer_handle;
@@ -45,21 +43,36 @@ void SimpleShapeApplication::init() {
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+    // Wypisujemy tyle elementów ile mamy wierzcho³ków
+    std::vector<GLushort> indices = {
+           0,1,2, // Dach
+           // Blok - kwadrat z dwóch trójk¹tów
+           3,4,5, // Lewy trójk¹t
+           5,4,6 // Prawy trójk¹t 
+    };
+    GLuint idx_buffer_handle;
+    glGenBuffers(1, &idx_buffer_handle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(),
+        GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     glGenVertexArrays(1, &vao_);
     glBindVertexArray(vao_);
-    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
 
-        // Pod³¹czenie pozycji wierzcho³ków
-        glEnableVertexAttribArray(0);
-        // Define an array of generic vertex attribute data (index, size, type, normalized, stride, pointer)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
+        glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
 
-        // Pod³¹czenie kolorów wierzcho³ków
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid*>(3 * sizeof(GLfloat)));
+            // Pod³¹czenie pozycji wierzcho³ków
+            glEnableVertexAttribArray(0);
+            // Define an array of generic vertex attribute data (index, size, type, normalized, stride, pointer)
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+            // Pod³¹czenie kolorów wierzcho³ków
+            glEnableVertexAttribArray(1);
+            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid*>(3 * sizeof(GLfloat)));
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
 
@@ -74,6 +87,6 @@ void SimpleShapeApplication::init() {
 
 void SimpleShapeApplication::frame() {
     glBindVertexArray(vao_);
-    glDrawArrays(GL_TRIANGLES, 0, 9); // Render primitives from array data (mode, first, count);
+    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_SHORT, nullptr);
     glBindVertexArray(0);
 }
