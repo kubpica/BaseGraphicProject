76852357042a86272102150141792d0cf529283b
Kubpica <kubpicapf@gmail.com>
2020-11-24 03:21:54 +0100

    Prep to "Resize" exercise

---
+++ src/Exercises/Resize/shaders/base_vs.glsl
@@ -0,0 +1,20 @@
+#version 410
+
+
+layout(location=0) in  vec4 a_vertex_position;
+layout(location=1) in vec3 a_vertex_color;
+
+layout(std140) uniform Transformations 
+{
+    mat4 P;
+    mat4 V;
+};
+
+out vec3 vertex_color;
+
+void main() 
+{
+    vertex_color = a_vertex_color;
+    
+    gl_Position = P*V*a_vertex_position;
+}
