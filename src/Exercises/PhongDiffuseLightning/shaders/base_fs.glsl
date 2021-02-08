ed437b580e0b08fbaa3bf72f6c3e00ffe1c0ec67
Kubpica <kubpicapf@gmail.com>
2021-02-08 02:26:51 +0100

    Oświetlenie dyfuzyjne

--- src/Exercises/PhongDiffuseLightning/shaders/base_fs.glsl
+++ src/Exercises/PhongDiffuseLightning/shaders/base_fs.glsl
@@ -18,5 +18,15 @@ uniform sampler2D diffuse_map;
 
 void main() 
 {
-    vFragColor = texture(diffuse_map, texture_uv);
+    // Po interpolacji jego d³ugoœæ mog³a ulec zmianie, dlatego musimy go znormalizowaæ na nowo
+    vec3 normal = normalize(vertex_normal_in_vs);
+    // wektor "do œwiat³a"
+    vec3 light_vector = normalize(light.position_in_vs - vertex_position_in_vs);
+    // natê¿enie œwiat³a padaj¹cego
+    float light_in = max(0.0,dot(normal, light_vector));
+
+    // przypisanie pikselowi koloru
+    vec4 diffuse_color = texture(diffuse_map, texture_uv);
+    vFragColor.a = diffuse_color.a;
+    vFragColor.rgb = light_in * diffuse_color.rgb*light.color;
 }
