0a014d1385ef8f3e6cced0460fc5d84a1c2374e9
Kubpica <kubpicapf@gmail.com>
2021-02-08 01:45:51 +0100

    Przekazywanie zmiennych do shadera fragmentów.

--- src/Exercises/PhongDiffuseLightning/shaders/base_fs.glsl
+++ src/Exercises/PhongDiffuseLightning/shaders/base_fs.glsl
@@ -4,6 +4,9 @@
 layout(location=0) out vec4 vFragColor;
 in vec2 texture_uv;
 
+in vec3 vertex_normal_in_vs;
+in vec3 vertex_position_in_vs;
+
 uniform sampler2D diffuse_map; 
 
 void main() 
--- src/Exercises/PhongDiffuseLightning/shaders/base_vs.glsl
+++ src/Exercises/PhongDiffuseLightning/shaders/base_vs.glsl
@@ -14,9 +14,17 @@ layout(std140) uniform Transformations
 
 out vec2 texture_uv;
 
+// Potrzebne zmienne przekszta³cone do przestrzeni widoku musimy przekazaæ do shadera fragmentów
+out vec3 vertex_normal_in_vs;
+out vec3 vertex_position_in_vs; 
+
 void main() 
 {
     texture_uv = a_texture_uv;
     
     gl_Position = P * VM * a_vertex_position;
+
+    vertex_position_in_vs = (VM*a_vertex_position).xyz;
+    // W przypadku wektora normalnego bezpiecznie jest go jeszcze unormowaæ po przekszta³ceniu
+    vertex_normal_in_vs = normalize(N*a_vertex_normal);
 }
